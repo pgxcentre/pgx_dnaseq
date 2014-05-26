9f6b9c06a00f9619cada5281d8fcc5d44b419f7d
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2014-05-26 11:25:03 -0400

    Added the coverage graph (per sample) to the pipeline

--- pgx_dna_seq/tool/pgx_coverage_graph.py
+++ pgx_dna_seq/tool/pgx_coverage_graph.py
@@ -1,4 +1,4 @@
-__all__ = ["CoverageGraph_Multi"]
+__all__ = ["CoverageGraph", "CoverageGraph_Multi"]
 
 import re
 
@@ -17,6 +17,54 @@ class PGx_CoverageGraph(GenericTool):
     def __init__(self):
         """Initialize a PGx_CoverageGraph instance."""
         pass
+
+
+class CoverageGraph(PGx_CoverageGraph):
+
+    # The name of the tool
+    _tool_name = "CoverageGraph"
+
+    # The options
+    _command = "{other_opt} --out {out_prefix} --bam {input}"
+
+    # The STDOUT and STDERR
+    _stdout = "{output}.out"
+    _stderr = "{output}.err"
+
+    # The description of the required options
+    _required_options = {"input":      GenericTool.INPUT,
+                         "output":     GenericTool.OUTPUT,
+                         "out_prefix": GenericTool.OUTPUT,
+                         "other_opt":  GenericTool.OPTIONAL}
+
+    # The suffix that will be added just before the extension of the output file
+    _suffix = "coverage_graph"
+
+    # The input and output type
+    _input_type = (r"\.(\S+\.)?[sb]am$", )
+    _output_type = (".{}.png".format(_suffix), )
+
+    # This tool does not produce usable data...
+    _produce_data = False
+
+    def __init__(self):
+        """Initialize a CoverageGraph instance."""
+        pass
+
+    def execute(self, options, out_dir=None):
+        """Indexes all the BAM files and plots the coverage."""
+        # First we index all the input files
+        if "input" not in options:
+            m = "{}: no input file".format(self.__class__.__name__)
+            raise ProgramError(m)
+        IndexBam().execute({"input": options["input"]}, out_dir)
+
+        # Searching for the out prefix
+        out_prefix = re.sub("\.png$", "", options["output"])
+        options["out_prefix"] = out_prefix
+
+        # Executing the software
+        super(CoverageGraph, self).execute(options, out_dir)
 
 
 class CoverageGraph_Multi(PGx_CoverageGraph):
