9880dc7ea8f74ef5b8f93bb000ded27e835167e1
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2014-05-27 16:22:51 -0400

    Updated the setup.py to include all scripts and the requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@
 import os
 from setuptools import setup
 
+scripts = ["coverage_graph.py", "execute_pipeline.py"]
+
 setup(name="pgx_dna_seq",
       version="0.1",
       description="Automatic PGx DNA sequencing pipeline",
@@ -15,8 +17,9 @@ setup(name="pgx_dna_seq",
       author_email="louis-philippe.lemieux.perreault@statgen.org",
       url="http://www.statgen.org",
       license="GPL",
-##       scripts=[os.path.join("scripts", "manhattan_generator"),],
-##       install_requires=["matplotlib >=1.3.1", "numpy >= 1.8.0"],
+      scripts=[os.path.join("scripts", script_name) for script_name in scripts],
+      install_requires=["numpy >= 1.8.1", "pandas >= 0.13.1", "ruffus >= 2.4.1", 
+                        "matplotlib >=1.3.1"],
       classifiers=['Operating System :: Linux',
                    'Programming Language :: Python',
                    'Programming Language :: Python :: 3.4'])
