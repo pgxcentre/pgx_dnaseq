0aea28de8cdcf8404c82b28c8bb05f9381203e8b
abdellad <Daghrach.Abdellatif@pharmacogenomics.ca>
2014-10-27 15:34:31 -0400

    added standard deviation and mean values

--- test/automatic_report.py
+++ test/automatic_report.py
@@ -69,6 +69,11 @@ def check_args(args):
 
     return True
 
+def std_dev(list):
+    [float(i) for i in list]
+    m=sum(list)/len(list)
+    z=sum(([(x-m)**2 for x in list]))/len(list)
+    return z**0.5
 
 def parse_args():
     """Parses the command line options and arguments.
@@ -93,7 +98,28 @@ def parse_args():
 
 def print_report(what_to_run,sample_list):
     rmlist = ['report.toc','report.lof','report.out','report.log','report.aux']
-    pages =list ()
+    pages=list ()
+    val_total_read= list ()
+    val_too_short= list ()
+    val_trim1= list ()
+    val_trim2= list ()
+    val_Total_Reads= list ()
+    val_Total_Duplicate= list ()
+    val_Duplicate_Per= list ()
+    val_Optical_Duplicate= list ()
+    val_PCR_Duplicate= list ()
+    val_Off_Bait= list ()
+    val_Mean_Bait_Coverage= list ()
+    val_Mean_T_Coverage= list ()
+    val_Fold_Enrichement= list ()
+    val_Zero_CVG_Target= list ()
+    val_PCT_T_BASES_2X= list ()
+    val_PCT_T_BASES_10X= list ()
+    val_PCT_T_BASES_20X= list ()
+    val_PCT_T_BASES_30X= list ()
+    val_PCT_T_BASES_40X= list ()
+    val_PCT_T_BASES_50X= list ()
+    val_PCT_T_BASES_100X= list ()
     for sample in  (sample_list):   
 
         for job_index, (job, job_options) in enumerate(what_to_run):
@@ -105,21 +131,35 @@ def print_report(what_to_run,sample_list):
                     myfile=open (hsmetrics_file)
                     data= list ()
                     for i, line in enumerate (myfile):
-                        if i>6:
+                        if i==7:
                             data.append(line.replace("_","-").rstrip("\r\n").split("\t"))
                             Total_Reads=data[0][5]
                             Off_Bait=data[0][15]                
                             Mean_Bait_Coverage=data[0][20]      
                             Fold_Enrichement=data[0][24]        
-                            Mean_Target_Coverage=data[0][21]    
+                            Mean_T_Coverage=data[0][21]    
                             Zero_CVG_Target=data[0][25]         
-                            PCT_TARGET_BASES_2X=data[0][27]     
-                            PCT_TARGET_BASES_10X=data[0][28]    
-                            PCT_TARGET_BASES_20X=data[0][29]    
-                            PCT_TARGET_BASES_30X=data[0][30]    
-                            PCT_TARGET_BASES_40X=data[0][31]    
-                            PCT_TARGET_BASES_50X=data[0][32]    
-                            PCT_TARGET_BASES_100X=data[0][33]   
+                            PCT_T_BASES_2X=data[0][27]     
+                            PCT_T_BASES_10X=data[0][28]    
+                            PCT_T_BASES_20X=data[0][29]    
+                            PCT_T_BASES_30X=data[0][30]    
+                            PCT_T_BASES_40X=data[0][31]    
+                            PCT_T_BASES_50X=data[0][32]    
+                            PCT_T_BASES_100X=data[0][33]   
+                            
+                            val_Total_Reads.append(float(Total_Reads))
+                            val_Off_Bait.append(float(Off_Bait))
+                            val_Mean_Bait_Coverage.append(float(Mean_Bait_Coverage))
+                            val_Fold_Enrichement.append(float(Fold_Enrichement))
+                            val_Mean_T_Coverage.append(float(Mean_T_Coverage))
+                            val_Zero_CVG_Target.append(float(Zero_CVG_Target))
+                            val_PCT_T_BASES_2X.append(float(PCT_T_BASES_2X))
+                            val_PCT_T_BASES_10X.append(float(PCT_T_BASES_10X))
+                            val_PCT_T_BASES_20X.append(float(PCT_T_BASES_20X))
+                            val_PCT_T_BASES_30X.append(float(PCT_T_BASES_30X))
+                            val_PCT_T_BASES_40X.append(float(PCT_T_BASES_40X))
+                            val_PCT_T_BASES_50X.append(float(PCT_T_BASES_50X))
+                            val_PCT_T_BASES_100X.append(float(PCT_T_BASES_100X))
 
          #Duplicate 
             if job.get_tool_name() ==  'MarkDuplicates' :
@@ -127,15 +167,19 @@ def print_report(what_to_run,sample_list):
                     myfile=open (dedup_file)
                     data= list ()
                     for i, line in enumerate (myfile):
-                        if i>6:
+                        if i==7:
                             data.append(line.replace("_","-").rstrip("\r\n").split("\t"))
                             Total_Duplicate=data[0][5]
-                            Optical_Duplicate=str (int(data[0][6]) * 100 / int(Total_Duplicate))
+                            Optical_Duplicate=str (int(data[0][6]) * 100 / float(Total_Duplicate))
                             PCR_Duplicate=str(100-float(Optical_Duplicate))
-                            Duplicate_Percentage=str(float(data[0][7]) * 100 )
+                            Duplicate_Per=str(float(data[0][7]) * 100 )
+                            
+                            val_Total_Duplicate.append(float(data[0][5]))
+                            val_Optical_Duplicate.append(int(data[0][6])*100 / int(Total_Duplicate))
+                            val_PCR_Duplicate.append(100-float(Optical_Duplicate))
+                            val_Duplicate_Per.append(float(data[0][7])*100)
          #Coverage_graph
             if job.get_tool_name() ==  'CoverageGraph' :
-                    print (sample)
                     coverage_graph = glob.glob(output_dir +'/'+sample+'*.png')[0]
                     coverage_graph=coverage_graph.replace(".png",".png").replace("/"+sample,"/"+sample)
                     shutil.copy(coverage_graph, output_dir +'/'+sample+'.png')
@@ -161,7 +205,11 @@ def print_report(what_to_run,sample_list):
                         too_short=line1.split(" ")[4]
                         trim1=line2.split(" ")[1]
                         trim2=line3.split(" ")[1]
-   
+                        
+                        val_total_read.append(int(line.split(" ")[2]))
+                        val_too_short.append(int(line1.split(" ")[4]))
+                        val_trim1.append(int(line2.split(" ")[1]))
+                        val_trim2.append(int(line3.split(" ")[1]))
     #Sample page  
         page="\\begin{landscape} \n\
 \subsection{"+sample+"} \n\
@@ -181,23 +229,23 @@ Trimmed R2              & "+ trim2 +"  \\\ \n\
 \\\\\\\\ \n\
 \multicolumn{3}{l}{HS Metrics} \\\ \n\
 \hline \n\
-Total Reads             & "+Total_Reads+"  \\\ \n\
-Total Duplicate         & "+Total_Duplicate+"\%  \\\ \n\
-Duplicate Percentage    & "+Duplicate_Percentage+"\%   \\\ \n\
-Optical Duplicate       & "+Optical_Duplicate+"\%   \\\ \n\
-PCR Duplicate           & "+PCR_Duplicate+"\%   \\\ \n\
-Off-Bait                & "+Off_Bait+"\%   \\\ \n\
-Mean Bait Coverage      & "+Mean_Bait_Coverage+"   \\\ \n\
-Mean Target Coverage    & "+Mean_Target_Coverage+"   \\\ \n\
-Fold Enrichement        & "+Fold_Enrichement+"   \\\ \n\
-Zero CVG Target         & "+Zero_CVG_Target+"\%  \\\ \n\
-PCT-TARGET-BASES-2X     & "+PCT_TARGET_BASES_2X+"\%   \\\ \n\
-PCT-TARGET-BASES-10X    & "+PCT_TARGET_BASES_10X+"\%   \\\ \n\
-PCT-TARGET-BASES-20X    & "+PCT_TARGET_BASES_20X+"\%   \\\ \n\
-PCT-TARGET-BASES-30X    & "+PCT_TARGET_BASES_30X+"\%   \\\ \n\
-PCT-TARGET-BASES-40X    & "+PCT_TARGET_BASES_40X+"\%   \\\ \n\
-PCT-TARGET-BASES-50X    & "+PCT_TARGET_BASES_50X+"\%   \\\ \n\
-PCT-TARGET-BASES-100X   & "+PCT_TARGET_BASES_100X+"\%   \\\ \n\
+Total Reads             & "+str("%.2f" % float(Total_Reads))+"  \\\ \n\
+Total Duplicate         & "+str("%.2f" % float( Total_Duplicate))+"  \\\ \n\
+Duplicate Percentage    & "+str("%.2f" % float(Duplicate_Per))+"\%   \\\ \n\
+Optical Duplicate       & "+str("%.2f" % float(Optical_Duplicate))+"\%   \\\ \n\
+PCR Duplicate           & "+str("%.2f" % float(PCR_Duplicate))+"\%   \\\ \n\
+Off-Bait                & "+str("%.2f" % float(Off_Bait))+"   \\\ \n\
+Mean Bait Coverage      & "+str("%.2f" % float(Mean_Bait_Coverage))+"   \\\ \n\
+Mean Target Coverage    & "+str("%.2f" % float(Mean_T_Coverage))+"   \\\ \n\
+Fold Enrichement        & "+str("%.2f" % float(Fold_Enrichement))+"   \\\ \n\
+Zero CVG Target         & "+str("%.2f" % float(Zero_CVG_Target))+"\%  \\\ \n\
+PCT-TARGET-BASES-2X     & "+str("%.2f" % float(PCT_T_BASES_2X))+"\%   \\\ \n\
+PCT-TARGET-BASES-10X    & "+str("%.2f" % float(PCT_T_BASES_10X))+"\%   \\\ \n\
+PCT-TARGET-BASES-20X    & "+str("%.2f" % float(PCT_T_BASES_20X))+"\%   \\\ \n\
+PCT-TARGET-BASES-30X    & "+str("%.2f" % float(PCT_T_BASES_30X))+"\%   \\\ \n\
+PCT-TARGET-BASES-40X    & "+str("%.2f" % float(PCT_T_BASES_40X))+"\%   \\\ \n\
+PCT-TARGET-BASES-50X    & "+str("%.2f" % float(PCT_T_BASES_50X))+"\%   \\\ \n\
+PCT-TARGET-BASES-100X   & "+str("%.2f" % float(PCT_T_BASES_100X))+"\%   \\\ \n\
 \hline \n\
 \\\\ \n\
 \hline \n\
@@ -209,15 +257,14 @@ Insert Size Std Dev     &         & $\pm$ 3 \\\ \n\
 \end{minipage}%\n\
 \\begin{minipage}[c][6in]{5in}\n\
 \centering \n\
-\includegraphics[width=4.5in]{"+coverage_graph_multi+"}\\\ \n\
+\includegraphics[width=4.5in]{"+coverage_graph+"}\\\ \n\
 \\vfill \n\
-\includegraphics[width=4.5in]{"+coverage_graph_multi+"} \n\
+\includegraphics[width=4.5in]{"+coverage_graph+"} \n\
 \end{minipage} \n\
 \end{minipage} \n\
 \end{landscape} \n\
 \n \
 "
-    #    print (page)       
         
         pages.append(page) 
     
@@ -359,11 +406,66 @@ the schematic of the pipeline used for run \RunName. \n\
 \end{document} " 
 
     
-
-
+#all samples page
+    sample_num=len(sample_list)
+    all_samples="\\begin{landscape} \n\
+\subsection{all\_sample} \n\
+\\begin{minipage}[c][6in]{9.3in} \n\
+\centering \n\
+\\begin{minipage}[c][6in]{4in}\n\
+\centering\n\
+\\begin{tabular}{lrl}\n\
+\multicolumn{3}{l}{Clipping/Trimming} \\\ \n\
+\n\
+\hline \n\
+Total read              & "+ str("%.2f"  %(sum(val_total_read)/sample_num)) +"& $\pm$ "+str("%.2f"  % std_dev(val_total_read))+"  \\\ \n\
+Too short after clip    & "+ str("%.2f" % (sum(val_too_short)/sample_num)) +" & $\pm$ "+str("%.2f"  % std_dev(val_too_short))+"\\\ \n\
+Trimmed R1              & "+ str("%.2f" % (sum(val_trim1)/sample_num)) +" & $\pm$ "+str("%.2f"  % std_dev(val_trim1))+"\\\ \n\
+Trimmed R2              & "+ str("%.2f" % (sum(val_trim2)/sample_num)) +" & $\pm$ "+str("%.2f"  % std_dev(val_trim2))+" \\\ \n\
+\hline \n\
+\\\\\\\\ \n\
+\multicolumn{3}{l}{HS Metrics} \\\ \n\
+\hline \n\
+Total Reads             & "+str("%.2f" % (sum(val_Total_Reads)/sample_num))+" & $\pm$ "+str("%.2f"  % std_dev(val_Total_Reads))+"\\\ \n\
+Total Duplicate         & "+str("%.2f" %(sum(val_Total_Duplicate)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_Total_Duplicate))+"\\\ \n\
+Duplicate Percentage    & "+str("%.2f" %(sum(val_Duplicate_Per)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_Duplicate_Per))+" \\\ \n\
+Optical Duplicate       & "+str("%.2f" % (sum(val_Optical_Duplicate)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_Optical_Duplicate))+" \\\ \n\
+PCR Duplicate           & "+str("%.2f" % (sum(val_PCR_Duplicate)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_PCR_Duplicate))+" \\\ \n\
+Off-Bait                & "+str("%.2f" % (sum(val_Off_Bait)/sample_num))+"  & $\pm$ "+str("%.2f"  % std_dev(val_Off_Bait))+" \\\ \n\
+Mean Bait Coverage      & "+str("%.2f" % (sum(val_Mean_Bait_Coverage)/sample_num))+"  & $\pm$ "+str("%.2f"  % std_dev(val_Mean_Bait_Coverage))+" \\\ \n\
+Mean Target Coverage    & "+str("%.2f" % (sum(val_Mean_T_Coverage)/sample_num))+"  & $\pm$ "+str("%.2f"  % std_dev(val_Mean_T_Coverage))+" \\\ \n\
+Fold Enrichement        & "+str("%.2f" % (sum(val_Fold_Enrichement)/sample_num))+"  & $\pm$ "+str("%.2f"  % std_dev(val_Fold_Enrichement))+" \\\ \n\
+Zero CVG Target         & "+str("%.2f" % (sum(val_Zero_CVG_Target)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_Zero_CVG_Target))+" \\\ \n\
+PCT-TARGET-BASES-2X     & "+str("%.2f" % (sum(val_PCT_T_BASES_2X)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_PCT_T_BASES_2X))+" \\\ \n\
+PCT-TARGET-BASES-10X    & "+str("%.2f" % (sum(val_PCT_T_BASES_10X)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_PCT_T_BASES_10X))+" \\\ \n\
+PCT-TARGET-BASES-20X    & "+str("%.2f" % (sum(val_PCT_T_BASES_20X)/sample_num))+"\%  & $\pm$  "+str("%.2f"  % std_dev(val_PCT_T_BASES_20X))+"\\\ \n\
+PCT-TARGET-BASES-30X    & "+str("%.2f" %(sum(val_PCT_T_BASES_30X)/sample_num))+"\%  & $\pm$ "+str("%.2f"  %std_dev(val_PCT_T_BASES_30X))+" \\\ \n\
+PCT-TARGET-BASES-40X    & "+str("%.2f" %(sum(val_PCT_T_BASES_40X)/sample_num))+"\%  & $\pm$ "+str("%.2f"  % std_dev(val_PCT_T_BASES_40X))+" \\\ \n\
+PCT-TARGET-BASES-50X    & "+str("%.2f"%(sum(val_PCT_T_BASES_50X)/sample_num))+"\%  & $\pm$ "+str("%.2f"  %std_dev(val_PCT_T_BASES_50X))+" \\\ \n\
+PCT-TARGET-BASES-100X   & "+str("%.2f"%(sum(val_PCT_T_BASES_100X)/sample_num))+"\%  & $\pm$ "+str("%.2f" %std_dev(val_PCT_T_BASES_100X))+"\\\ \n\
+\hline \n\
+\\\\ \n\
+\hline \n\
+Mean Insert Size        &        & $\pm$ 10 \\\ \n\
+Median Insert Size      &        & $\pm$ 9 \\\ \n\
+Insert Size Std Dev     &         & $\pm$ 3 \\\ \n\
+\hline \n\
+\end{tabular}\n\
+\end{minipage}%\n\
+\\begin{minipage}[c][6in]{5in}\n\
+\centering \n\
+\includegraphics[width=4.5in]{"+coverage_graph_multi+"}\\\ \n\
+\\vfill \n\
+\includegraphics[width=4.5in]{"+coverage_graph_multi+"} \n\
+\end{minipage} \n\
+\end{minipage} \n\
+\end{landscape} \n\
+\n \
+"
 #generate tex
     report = open("report.tex", "w")
     report.write(start)
+    report.write(all_samples)
     for  i,p in enumerate (pages):
         report.write (p)
     report.write (end )
@@ -413,7 +515,6 @@ try:
     run_name="Sequencing_Run"
     run_name=args.run_name
     run_name=run_name.replace("_", "\_")
-    print (run_name)
     # Checking the input files
     input_files,sample_list = check_input_files(args.input)
      
