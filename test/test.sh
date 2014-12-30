5d2af1cc4b73d74500b23ad1b26ace151feecd4e
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2014-12-30 13:08:22 -0500

    Test includes the automatic report creation

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-PYTHONPATH=..:$PYTHONPATH \
-../scripts/execute_pipeline.py \
+execute_pipeline.py \
     --input input_files.txt \
     --tool-config tool.conf \
+    -n 2 \
     --pipeline-config pipeline.conf
+
+rc=$?
+if [[ $rc == 0 ]]
+then
+    automatic_report.py \
+        --input input_files.txt \
+        --pipeline-config pipeline.conf \
+        --run-name "Dummy_Run"
+fi
+
