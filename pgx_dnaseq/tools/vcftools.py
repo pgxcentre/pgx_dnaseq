de30bd3d01a34917a56a7d7ff9537648c8697c89
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-02-11 13:59:20 -0500

    Started to add vcftools

---
+++ pgx_dnaseq/tools/vcftools.py
@@ -0,0 +1,67 @@
+
+# This file is part of pgx_dnaseq
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative
+# Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+
+__author__ = "Louis-Philippe Lemieux Perreault"
+__copyright__ = ("Copyright 2015 Beaulieu-Saucier Universite de Montreal "
+                 "Pharmacogenomics Centre. All rights reserved.")
+__license__ = "Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)"
+
+
+import re
+
+from . import GenericTool
+from .. import ProgramError
+
+
+__all__ = ["VcfConcat"]
+
+
+class Vcftools(GenericTool):
+
+    # The version of the tool
+    _version = "0.1.12b"
+
+    # The executable
+    _exec = "vcftools"
+
+    def __init__(self):
+        """Initialize a Vcftools instance."""
+        pass
+
+
+class VcfConcat(Vcftools):
+
+    # The name of the tool
+    _tool_name = "VcfConcat"
+
+    # The executable is different
+    _exec = "vcf-concat"
+
+    # The options
+    _command = "{inputs}"
+
+    # The STDOUT and STDERR
+    _stdout = "{output}"
+    _stderr = "{output}.err"
+
+    # The description of the required options
+    _required_options = {"inputs":  GenericTool.INPUTS,
+                         "output": GenericTool.OUTPUT}
+
+    # The suffix that will be added just before the extension of the output
+    # file
+    _suffix = "vcf_concat"
+
+    # The input and output type
+    _input_type = (r"\.(\S+\.)?vcf$", )
+    _output_type = (".{}.vcf".format(_suffix), )
+
+    def __init__(self):
+        """Initialize a Sam2Bam instance."""
+        pass
